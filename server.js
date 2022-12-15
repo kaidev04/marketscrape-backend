86c1c0226a20326aca36d361de5ef389cbb90d31
kaidev04 <akakumura10@gmail.com>
2022-12-15 18:12:08 +0100

    set Access-Control-Allow-Origin headers from response

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const http = require('http').createServer();
 const { getSentiment } = require('./sentiment');
+const http = require('http').createServer(function(req, res) {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+});
 
 const io = require('socket.io')(http, {
     cors: { origin: '*' }
